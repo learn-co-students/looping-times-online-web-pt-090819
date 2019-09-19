c42811f983eb93bcbc8839d93e3d6e730b3ec64d
Hannah Harrington <hannahharrington@outlook.com>
2019-09-19 04:50:15 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,4 @@
 def using_times
-	#your code here
+	7.times
+	  puts "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
 end
-
-
-
-
